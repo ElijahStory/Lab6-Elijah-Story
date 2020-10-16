9a3ec235de0bc7fb516dee7b3f46685dd330c3fe
EaStory <56444910+EaStory@users.noreply.github.com>
2020-10-16 10:40:03 -0400

    Starting to code

--- Lab6_RGB_Button/Lab6_RGB_Button.ino
+++ Lab6_RGB_Button/Lab6_RGB_Button.ino
@@ -1,6 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+//Elijah Story
+//NMD211 Lab6
+//Button press LED on
+//10-16-2020
 
+//From: 
+//Press the button, the LED turns on.
+
+int ledPin = 13;  //LED power scorce
+int buttonIn = 7; //push button digital input
+
+int buttonStatus = 0; //pushed or not (0 is not)
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  //LED is output
+  pinMode(buttonIN, INPUT); //Button input/trigger
+  
+  Serial.begin(9600);
 }
 
 void loop() {
